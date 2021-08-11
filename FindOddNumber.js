ded91f4d967551e3f0d75de53d6dc6961f809a81
Lorna Kimani <kimanilorna@yahoo.com>
2021-08-11 10:26:55 +0300

    find odd number solution

---
+++ FindOddNumber.js
@@ -0,0 +1,11 @@
+function OddNumbers(A) {
+   
+        if (A%2 === 0) {
+            console.log("A is an even number")
+        }else{
+            console.log("A is an odd number")
+        }
+
+}
+
+OddNumbers(4)
