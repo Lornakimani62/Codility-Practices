ce23db9bcbae1244ee2f06e71483dd6256795eaa
Lorna Kimani <kimanilorna@yahoo.com>
2021-08-13 10:55:01 +0300

    write more efficient algorithm for the tape equilibrium problem

--- TapeEquilibrium.js
+++ TapeEquilibrium.js
@@ -1,19 +1,19 @@
 function TapeEquilibrium(A){
-    //Divide the array into two Parts
-    SideA = A[0]
-    //Get the sum of the elements in Side B
-    SideB = A.reduce(function(a, b) { return a + b; }, 0) - SideA
-    //Get the absolute difference between side A and side B
-    Sum = Math.abs(SideA - SideB)
-    for(i=1;i<A.length-1;i++){
-        SideA += A[i]
-        SideB -= A[i]
+ SideA = 0
+ SideB = A.reduce((a,b) => a+b)
+ answer = null
 
-        tempSum = Math.abs(SideA - SideB)
-        if(tempSum < Sum){
-            Sum = tempSum
-        }
+for(i=0;i<A.length - 1;i++){
+    SideA += A[i]
+    SideB -= A[i]
+
+    diff = Math.abs(SideA - SideB)
+
+    if(answer == null || answer > diff){
+        answer = diff
     }
-    return Sum
 }
-TapeEquilibrium([3,1,2,4,3])
+ console.log(answer)
+ return answer
+}
+TapeEquilibrium([1,2,3])
